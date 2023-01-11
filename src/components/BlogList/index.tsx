44137aa5c4d57aa04048016bc9ef591bece615ef
zhanglun <zhanglun@yiwise.com>
2023-01-11 18:27:45 +0800

    improvement: update BlogList

--- src/components/BlogList/index.tsx
+++ src/components/BlogList/index.tsx
@@ -1,40 +1,66 @@
-import React from 'react';
-import type { MarkdownInstance } from 'astro';
-import type { Frontmatter } from '@types';
-import { SITE } from '@config';
-import Card from '@components/Card';
-import slugify from '@utils/slugify';
+import React, { useState } from "react";
+import type { MarkdownInstance } from "astro";
+import type { Frontmatter } from "@types";
+import { SITE } from "@config";
+import Card from "@components/Card";
+import slugify from "@utils/slugify";
+import { CircleArrow, CircleArrowThemeEnum } from "@components/CircleArrow";
 
 export interface Props {
   allPosts: MarkdownInstance<Frontmatter>[];
 }
 
 export const BlogList = (props: Props) => {
-  const { allPosts } = props
-  const renderPosts = allPosts.slice(0, SITE.postPerPage)
+  const { allPosts } = props;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [renderPosts, setRenderPosts] = useState(
+    allPosts.slice(0, SITE.postPerPage)
+  );
+  console.log("%c Line:17 🌮 renderPosts", "color:#e41a6a", renderPosts);
 
-  console.log("%c Line:15 🍑 renderPosts", "color:#ed9ec7", renderPosts);
+  function viewMore() {
+    if (renderPosts.length >= allPosts.length) {
+      return;
+    }
 
-  return <div>
-    <ul className="grid w-full lg:gap-y-14 lg:grid-cols-[repeat(12,1fr)] lg:gap-6">
-      {renderPosts.map(({ frontmatter, compiledContent }) => {
-        return (
-          <Card
-            href={`/blogs/${slugify(frontmatter)}`}
-            frontmatter={frontmatter}
-            rawContent={compiledContent()}
-          />
-        );
-      })
-      }
-    </ul>
+    const updatePage = currentPage + 1;
 
-    { renderPosts.length < allPosts.length && (
-      <nav className="pagination-wrapper" aria-label="Pagination">
-        <div>
-          <span>VIEW MORE BLOGS</span>
-        </div>
-      </nav>
-    )}
-  </div>
-}
+    setCurrentPage(updatePage);
+    setRenderPosts(allPosts.slice(0, updatePage * SITE.postPerPage));
+
+    console.log(
+      "%c Line:15 🍑 renderPosts",
+      "color:#ed9ec7",
+      renderPosts.length
+    );
+  }
+
+  return (
+    <div>
+      <ul className="grid w-full lg:gap-y-14 lg:grid-cols-[repeat(12,1fr)] lg:gap-6">
+        {renderPosts.map(({ frontmatter, compiledContent }) => {
+          return (
+            <Card
+              href={`/blogs/${slugify(frontmatter)}`}
+              frontmatter={frontmatter}
+              rawContent={compiledContent()}
+            />
+          );
+        })}
+      </ul>
+
+      {renderPosts.length < allPosts.length && (
+        <nav className="pagination-wrapper" aria-label="Pagination">
+          <div onClick={() => viewMore()}>
+            <span>VIEW MORE BLOGS</span>
+            <CircleArrow
+              size={1.2}
+              rotate={0}
+              theme={CircleArrowThemeEnum.light}
+            />
+          </div>
+        </nav>
+      )}
+    </div>
+  );
+};
--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE = {
   description: "A clean, responsive and SEO-friendly Astro blog theme.",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 15,
+  postPerPage: 5,
   social: {
     twitter: "zhanglun1410",
   },
