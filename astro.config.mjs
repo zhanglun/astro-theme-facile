15e6c59e19b02bac1ce90230775a5229d87174c4
zhanglun <zhanglun@yiwise.com>
2023-01-12 09:10:37 +0800

    feat: add Github Action

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-paper.pages.dev/",
+  site: "https://https://zhanglun.github.io",
+  base: "/astro-theme-facile",
   integrations: [
     tailwind({
       config: {
