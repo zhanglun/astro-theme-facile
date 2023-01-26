8abe26289fa45fc4c36e96013cfb9e6fe18f5bd6
zhanglun <zhanglun1410@gmail.com>
2023-01-26 12:49:59 +0800

    feat: format date with dayjs

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -1,3 +1,5 @@
+import dayjs, { Dayjs } from "dayjs";
+
 export interface Props {
   datetime: string;
   size?: "sm" | "lg";
@@ -6,7 +8,9 @@ export interface Props {
 
 export default function Datetime({ datetime, size = "sm", className }: Props) {
   return (
-    <div className={`font-bold opacity-80 flex items-center justify-center space-x-2 ${className}`}>
+    <div
+      className={`font-bold opacity-80 flex items-center justify-center space-x-2 ${className}`}
+    >
       <span className="sr-only">Posted on:</span>
       <span className={`${size === "sm" ? "text-base" : "text-xl"}`}>
         <FormattedDatetime datetime={datetime} />
@@ -17,24 +21,8 @@ export default function Datetime({ datetime, size = "sm", className }: Props) {
 
 const FormattedDatetime = ({ datetime }: { datetime: string }) => {
   const myDatetime = new Date(datetime);
-  
-  const date = myDatetime.toLocaleDateString([], {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-  });
 
-  const time = myDatetime.toLocaleTimeString([], {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
+  const date = dayjs(datetime).format("YYYY-MM-D HH:mm");
 
-  return (
-    <>
-      {date}
-      <span aria-hidden="true"> | </span>
-      <span className="sr-only">&nbsp;at&nbsp;</span>
-      {time}
-    </>
-  );
+  return <>{date}</>;
 };
